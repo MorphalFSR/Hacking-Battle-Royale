d1eab89a556f9f63cb5b1bfeaf52029cbeafe1d0
Avivi <alonsegal2006@gmail.com>
2023-04-04 11:34:20 +0300

    renamed client file.

--- client.py
+++ client.py
@@ -33,7 +33,7 @@ class BlankFrame(Frame):
         self.root = root
 
 
-class Game(Tk):
+class ClientApp(Tk):
 
     def __init__(self):
 
@@ -59,5 +59,5 @@ class Game(Tk):
 
 
 if __name__ == "__main__":
-    game = Game()
+    game = ClientApp()
     game.mainloop()
