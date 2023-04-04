d345a71143149671e7435d7d8ded944d3cb334f3
Avivi <alonsegal2006@gmail.com>
2023-04-04 15:45:50 +0300

    added wordle-like password guessing interface

--- client.py
+++ client.py
@@ -5,35 +5,84 @@ import threading
 HOST = '127.0.0.1'
 PORT = 25252
 
+APPROVED = 'APPROVED'  # Correct username and password
+ICUSER = 'ICUSER'  # InCorrect Username
+ICPASS = 'ICPASS'  # InCorrect Password
+
 INTERACTABLE_FONT = ("Comic Sans MS", 12)
 END = 'end'
 WHITE = 'white'
 CENTER = 'center'
 
+COLORS = {'g': "#00FF00",
+          'y': "#FFFF00",
+          'p': "#FF00FF",
+          'r': "#FF0000"}
 
-class LoginFrame(Frame):
 
-    def __init__(self, parent, root):
-        super().__init__(master=parent)
+class BaseFrame(Frame):
+
+    def __init__(self, master, root):
+        super().__init__(master=master)
 
         self.root = root
+        self.entries = []
 
-        self.input_box = Entry(self, width=25, bg=WHITE, font=INTERACTABLE_FONT)
-        self.input_box.place(relx=0.5, rely=0.5, anchor=CENTER)
+    def load(self):
+        for box in self.entries:
+            box.delete(0, END)
+        self.tkraise()
 
-        self.master.update()
+
+class LoginFrame(BaseFrame):
+
+    def __init__(self, parent, root):
+        super().__init__(master=parent, root=root)
+
+        self.attempts = 0
+
+        self.password_box = Entry(self, width=25, bg=WHITE, font=INTERACTABLE_FONT)
+        self.password_box.place(relx=0.5, rely=0.5, anchor=CENTER)
+        self.entries.append(self.password_box)
+
+        self.update_idletasks()
+
+        self.username_box = Entry(self, width=25, bg=WHITE, font=INTERACTABLE_FONT)
+        self.username_box.place(relx=0.5, rely=0.5, y=-self.password_box.winfo_height() - 10, anchor=CENTER)
+        self.entries.append(self.username_box)
 
         self.sign_in_button = Button(self, width=8, height=1, font=INTERACTABLE_FONT, text="Sign in",
-                                     command=lambda: self.root.socket.send("Pressed".encode()))
-        self.sign_in_button.place(relx=0.5, rely=0.5, x=0, y=1.5 * self.input_box.winfo_height(), anchor=CENTER)
+                                     command=lambda: self.try_login(username=self.username_box.get(),
+                                                                    password=self.password_box.get()))
+        self.sign_in_button.place(relx=0.5, rely=0.5, y=0.5*self.password_box.winfo_height() + 10, anchor=N)
+
+    def try_login(self, username, password):
+        response = self.root.try_login(username, password).split(' ')
+        key = response[0]
+        args = response[1:]
+        if key == ICPASS:
+            for i in range(len(args)):
+                char = args[i][0]
+                colors = [COLORS[c] for c in args[i][1:]]
+                char_box = Label(self, bg=WHITE if len(colors) == 0 else colors[0], font=INTERACTABLE_FONT, height=1,
+                                 width=1, text=char)
+                char_box.pack()
+                self.update_idletasks()
+                char_box.place(x=self.password_box.winfo_x() + i * char_box.winfo_width(),
+                               y=self.sign_in_button.winfo_y() + self.sign_in_button.winfo_height() + 10 + self.attempts * (char_box.winfo_height() + 10),
+                               anchor=NW)
+
+            self.attempts += 1
 
 
-class BlankFrame(Frame):
+class InAppFrame(BaseFrame):
 
     def __init__(self, parent, root):
-        super().__init__(master=parent)
+        super().__init__(master=parent, root=root)
 
-        self.root = root
+        self.back_button = Button(self, width=5, height=1, font=INTERACTABLE_FONT, text="Back",
+                                  command=lambda: self.root.show_frame(LoginFrame.__name__))
+        self.back_button.pack()
 
 
 class ClientApp(Tk):
@@ -45,29 +94,29 @@ class ClientApp(Tk):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect((HOST, PORT))
 
-        listen_thread = threading.Thread(target=self.listen)
-        listen_thread.start()
-
         container = Frame(self)
         container.pack(side=TOP, fill=BOTH, expand=True)
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
         self.frames = dict()
-        for F in (LoginFrame, BlankFrame):
+        for F in (LoginFrame, InAppFrame):
             frame = F(parent=container, root=self)
             frame.grid(row=0, column=0, sticky="nsew")
             self.frames[F.__name__] = frame
 
-        self.show_frame("LoginFrame")
+        self.show_frame(LoginFrame.__name__)
 
     def show_frame(self, name):
-        self.frames[name].tkraise()
+        self.frames[name].load()
 
-    def listen(self):
-        while True:
-            data = self.socket.recv(1024)
-            print(data.decode())
+    def try_login(self, username, password):
+        self.socket.send(f"LOGIN {username} {password}".encode())
+        response = self.socket.recv(1024).decode()
+        if response == APPROVED:
+            print("Logging in.")
+            self.show_frame(InAppFrame.__name__)
+        return response
 
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -1,18 +1,64 @@
 import socket
 import threading
 
+APPROVED = 'APPROVED'  # Correct username and password
+ICUSER = 'ICUSER'  # InCorrect Username
+ICPASS = 'ICPASS'  # InCorrect Password
 
 IP = '0.0.0.0'
 PORT = 25252
 N_PLAYERS = 8
 
+PASSWORDS = {"a": "pass12345"}
+
+
+def hint_password(correct, attempt):
+    """generates wordle-like packet based on the entered password and the correct one.
+    message is in the form of 'ICPASS [character][colors] [character][colors]'...
+    where the colors indicate the hints for each characters:
+        g = green = correct
+        y = yellow = exists in password
+        p = purple = adjacent to the correct character in the ASCII table
+        r = red = at the end of the string, indicates incorrect length"""
+
+    message = ICPASS
+    for i in range(len(attempt)):
+        if i == len(correct):
+            message += ' ' + attempt[i] + 'r'
+            return message.encode()
+        message += " " + attempt[i]
+        if attempt[i] == correct[i]:
+            message += 'g'
+        else:
+            if attempt[i] in correct:
+                message += 'y'
+            if abs(ord(attempt[i]) - ord(correct[i])) == 1:
+                message += 'p'
+    if len(attempt) < len(correct):
+        message += " \0r"
+    return message.encode()
+
 
 def handle_client(client):
+    attempts = {user: 0 for user in PASSWORDS.keys()}
     with client:
         while True:
-            data = client.recv(1024)
-            print(data.decode())
-            client.send(data)
+            data = client.recv(1024).decode().split(" ")
+            command = data[0]
+            args = data[1:]
+            print(args)
+            if command == "LOGIN":
+                username, password = args
+                if username in PASSWORDS.keys():
+                    if PASSWORDS[username] == password:
+                        client.send(APPROVED.encode())
+                    else:
+                        print("pass")
+                        client.send(hint_password(PASSWORDS[username], password))
+                        attempts[username] += 1
+                else:
+                    print("user")
+                    client.send(ICUSER.encode())
 
 
 def main():
